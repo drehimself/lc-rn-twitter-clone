148be46feac05a6bfff97d727cb861689ccdab19
Andre Madarang <andre_madarang@hotmail.com>
2021-09-10 15:36:53 -0400

    Implement Design for Profile Screen

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,10 +1,233 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Linking,
+  FlatList,
+} from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
 
 export default function ProfileScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile Screen</Text>
+  const DATA = [
+    {
+      id: '1',
+      title: 'First Item',
+    },
+    {
+      id: '2',
+      title: 'Second Item',
+    },
+    {
+      id: '3',
+      title: 'Third Item',
+    },
+    {
+      id: '4',
+      title: 'Fourth Item',
+    },
+    {
+      id: '5',
+      title: 'Fifth Item',
+    },
+    {
+      id: '6',
+      title: 'Sixth Item',
+    },
+    {
+      id: '7',
+      title: 'Seventh Item',
+    },
+    {
+      id: '8',
+      title: 'Eight Item',
+    },
+    {
+      id: '9',
+      title: 'Ninth Item',
+    },
+    {
+      id: '10',
+      title: 'Tenth Item',
+    },
+  ];
+
+  const renderItem = ({ item }) => (
+    <View style={{ marginVertical: 20 }}>
+      <Text>{item.title}</Text>
     </View>
   );
+
+  const ProfileHeader = () => (
+    <View style={styles.container}>
+      <Image
+        style={styles.backgroundImage}
+        source={{
+          uri: 'https://images.unsplash.com/photo-1557683316-973673baf926?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80',
+        }}
+      />
+      <View style={styles.avatarContainer}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+        <TouchableOpacity style={styles.followButton}>
+          <Text style={styles.followButtonText}>Follow</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.nameContainer}>
+        <Text style={styles.profileName}>Andre Madarang</Text>
+        <Text style={styles.profileHandle}>@drehimself</Text>
+      </View>
+
+      <View style={styles.profileContainer}>
+        <Text style={styles.profileContainerText}>
+          CEO of CEOs. PhD, MSc, SEO, HTML, CSS, JS Evangelist Pro Expert S Rank
+          Elite Best of the best.
+        </Text>
+      </View>
+
+      <View style={styles.locationContainer}>
+        <EvilIcons name="location" size={24} color="gray" />
+        <Text style={styles.textGray}>Toronto, Canada</Text>
+      </View>
+
+      <View style={styles.linkContainer}>
+        <TouchableOpacity
+          style={styles.linkItem}
+          onPress={() => Linking.openURL('https://laracasts.com')}
+        >
+          <EvilIcons name="link" size={24} color="gray" />
+          <Text style={styles.linkColor}>laracasts.com</Text>
+        </TouchableOpacity>
+        <View style={[styles.linkItem, styles.ml4]}>
+          <EvilIcons name="calendar" size={24} color="gray" />
+          <Text style={styles.textGray}>Joined March 2009</Text>
+        </View>
+      </View>
+
+      <View style={styles.followContainer}>
+        <View style={styles.followItem}>
+          <Text style={styles.followItemNumber}>509</Text>
+          <Text style={styles.followItemLabel}>Following</Text>
+        </View>
+        <View style={[styles.followItem, styles.ml4]}>
+          <Text style={styles.followItemNumber}>2,354</Text>
+          <Text style={styles.followItemLabel}>Followers</Text>
+        </View>
+      </View>
+
+      <View style={styles.separator}></View>
+    </View>
+  );
+
+  return (
+    <FlatList
+      style={styles.container}
+      data={DATA}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+      ItemSeparatorComponent={() => <View style={styles.separator}></View>}
+      ListHeaderComponent={ProfileHeader}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  textGray: {
+    color: 'gray',
+  },
+  ml4: {
+    marginLeft: 16,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  backgroundImage: {
+    width: 800,
+    height: 120,
+  },
+  avatarContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    paddingHorizontal: 10,
+    marginTop: -34,
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    borderWidth: 4,
+    borderColor: 'white',
+  },
+  followButton: {
+    backgroundColor: '#0f1418',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 24,
+  },
+  followButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  nameContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 2,
+  },
+  profileName: {
+    fontWeight: 'bold',
+    fontSize: 22,
+  },
+  profileHandle: {
+    color: 'gray',
+    marginTop: 1,
+  },
+  profileContainer: {
+    paddingHorizontal: 10,
+    marginTop: 8,
+  },
+  profileContainerText: {
+    lineHeight: 22,
+  },
+  locationContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    marginTop: 12,
+  },
+  linkContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    marginTop: 4,
+  },
+  linkColor: {
+    color: '#1d9bf1',
+  },
+  linkItem: {
+    flexDirection: 'row',
+  },
+  followContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    paddingVertical: 12,
+  },
+  followItem: {
+    flexDirection: 'row',
+  },
+  followItemNumber: {
+    fontWeight: 'bold',
+  },
+  followItemLabel: {
+    marginLeft: 4,
+  },
+  separator: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#E5E7EB',
+  },
+});
