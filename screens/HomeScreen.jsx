01d325f2fe4d4c8d615e039517540d3a9836fe67
Andre Madarang <andre_madarang@hotmail.com>
2021-09-18 13:39:43 -0400

    Infinite scroll

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -22,17 +22,28 @@ export default function HomeScreen({ navigation }) {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isRefreshing, setIsRefreshing] = useState(false);
+  const [page, setPage] = useState(1);
+  const [isAtEndOfScrolling, setIsAtEndOfScrolling] = useState(false);
 
   useEffect(() => {
     getAllTweets();
-  }, []);
+  }, [page]);
 
   function getAllTweets() {
     axios
-      .get('http://lc-laravel-twitter-clone.test/api/tweets')
+      .get(`http://lc-laravel-twitter-clone.test/api/tweets?page=${page}`)
       .then(response => {
         // console.log(response.data);
-        setData(response.data);
+        if (page === 1) {
+          setData(response.data.data);
+        } else {
+          setData([...data, ...response.data.data]);
+        }
+
+        if (!response.data.next_page_url) {
+          setIsAtEndOfScrolling(true);
+        }
+
         setIsLoading(false);
         setIsRefreshing(false);
       })
@@ -44,8 +55,14 @@ export default function HomeScreen({ navigation }) {
   }
 
   function handleRefresh() {
+    setPage(1);
+    setIsAtEndOfScrolling(false);
     setIsRefreshing(true);
     getAllTweets();
+  }
+
+  function handleEnd() {
+    setPage(page + 1);
   }
 
   function gotoProfile() {
@@ -154,6 +171,13 @@ export default function HomeScreen({ navigation }) {
           )}
           refreshing={isRefreshing}
           onRefresh={handleRefresh}
+          onEndReached={handleEnd}
+          onEndReachedThreshold={0}
+          ListFooterComponent={() =>
+            !isAtEndOfScrolling && (
+              <ActivityIndicator size="large" color="gray" />
+            )
+          }
         />
       )}
       <TouchableOpacity
